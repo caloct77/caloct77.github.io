b2c39421d983a4e8c04fc55112351edf6349f99c
caloct77 <31992365+caloct77@users.noreply.github.com>
2020-12-23 00:09:54 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+Calvin's portfolio
+</head>
+<body>
+<img src="img/android.PNG" alt="Girl in a jacket" width="500" height="600"> 
+<img src="img/qc0.PNG" alt="Girl in a jacket" width="500" height="600"> 
+<img src="img/qc1.PNG" alt="Girl in a jacket" width="500" height="600"> 
+<img src="img/svrAgent.PNG" alt="Girl in a jacket" width="500" height="600"> 
+</body>
+</html>
